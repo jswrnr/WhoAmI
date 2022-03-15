a67a4839db393e598ec4a855eaaf632defa14140
jswrnr <64600096+jswrnr@users.noreply.github.com>
2022-03-15 22:23:54 +0100

    0.0.2
    addPlayer component
    switch from promise chain to async await
    tested vue setup syntax

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,19 @@
     <div v-if="player">Player name: {{this.player}}</div>
     <br>
     <player-list :playerList="this.passableList"></player-list>
+    <add-player @add="handleAdd($event)"></add-player>
 </template>
 
 <script>
 import PlayerList from "./components/PlayerList.vue";
 import InputName from "./components/InputName.vue";
+import AddPlayer from "./components/AddPlayer.vue";
+
 export default {
     components: {
         PlayerList,
-        InputName
+        InputName,
+        AddPlayer
     },
 
     data() {
@@ -32,19 +36,19 @@ export default {
     },
     methods : {
         //fetch the players list from server and update value in model
-        updateList() {
-           fetch(this.backend +"/players")
-           .then( res => {
-               if (!res.ok) {
-                   throw new Error("error fetching players "+ res.status );
-               }
-               return res.json()
-           })
-           .then( json => {
-               this.playerList = json
-           })
-           .catch( error => console.error(error))
-       }
+        async updateList() {
+            try {
+                let data = await fetch(this.backend +"/players");
+                this.playerList = await data.json();   
+            } catch (error) {
+                console.error("error fetching players");
+            }
+       },
+       //add a player to the list
+       //todo: push player to server instead
+       handleAdd(player) {
+           this.playerList.push(player)
+       } 
     },
     //on creation. update the players list
     created(){
---
+++ src/components/AddPlayer.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <label for="add">Add a player:</label>
+        <input type="text" id="name" v-model="name">
+        <input type="text" id="guess" v-model="guess">
+        <button @click="handleAdd()" id="add">Add</button>
+    </div>
+</template>
+
+<script setup>
+import { ref } from "@vue/reactivity";
+
+    const emit = defineEmits(["add"]);
+    const name = ref("");
+    const guess = ref("");
+    
+
+    function handleAdd() {
+        if (!name || !guess) {
+            console.log("can't add without name and guess");
+            return
+        }
+        console.log(name.value, guess.value);
+        emit("add", {"name": name.value, "guess": guess.value})
+    }
+</script>
